59614203e3cb67799072cc0a231c77c0d451620d
Boubou2555 <147002515+Boubou2555@users.noreply.github.com>
2026-01-01 02:11:21 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,25 @@
 import type { Metadata } from "next"
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Script from 'next/script'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Telegram Mini App',
-  description: 'Borhane Test App',
+  description: 'Borhane App',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar" dir="rtl">
       <head>
-        <Script 
-          src="https://telegram.org/js/telegram-web-app.js" 
-          strategy="beforeInteractive" 
-        />
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
         <Script 
           src="//libtl.com/sdk.js" 
           data-zone="10400479" 
-          data-sdk="show_10400479"
-          strategy="beforeInteractive"
+          data-sdk="show_10400479" 
+          strategy="beforeInteractive" 
         />
       </head>
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
