99f81e19ed4b7839352ac6ad527da46772af0e89
Boubou2555 <147002515+Boubou2555@users.noreply.github.com>
2026-01-01 01:58:53 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Telegram Mini App',
-  description: 'A simple Telegram mini app using Next.js and Prisma',
+  description: 'Borhane Test App',
 }
 
 export default function RootLayout({
@@ -18,19 +18,17 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <head>
-        {/* 1. سكربت تليجرام الأساسي - يتم تحميله قبل التفاعل */}
+        {/* سكربت تليجرام */}
         <Script 
           src="https://telegram.org/js/telegram-web-app.js" 
           strategy="beforeInteractive" 
         />
         
-        {/* 2. سكربت Monetag SDK - تم تحديثه بناءً على بيانات منطقتك (Zone 10400479) */}
-        {/* نستخدم استراتيجية beforeInteractive لضمان جاهزية دالة show_10400479 فور تحميل الصفحة */}
+        {/* سكربت Monetag - وضعناه بدون SDK تشغيل تلقائي لمنع الفتح العشوائي */}
         <Script 
-          src="//libtl.com/sdk.js" 
+          src="https://jsoque.com/sdk.js" 
           data-zone="10400479" 
-          data-sdk="show_10400479"
-          strategy="beforeInteractive"
+          strategy="lazyOnload"
         />
       </head>
       <body className={inter.className}>
